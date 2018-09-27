498148b3eab6b712326a90c1209ac9b0161c8275
Matilda Stevenson <matilda.stevenson@sap.com>
2018-09-27 17:43:31 +1000

    added procfile and app.json

--- scraper_server.py
+++ scraper_server.py
@@ -1,1 +1,11 @@
-import Flask
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return "Server working"
+
+
+@app.route('/analyse_feed',methods=['POST'])
