d98dcd9fd2a261779fe4b289fd80915226987ec8
Matilda Stevenson <matilda.stevenson@sap.com>
2018-09-27 17:37:14 +1000

    server file

--- recast_nlp.py
+++ recast_nlp.py
@@ -6,18 +6,13 @@ import recastai
 import json
 from twitter_scraper import scrape
 from collections import defaultdict
+import re
 
 DEV_TOKEN = "f9f4b8e63de484bac22545c58166442b"
 
 # URL to make recast requests to
 BASE_URL = "https://api.recast.ai/v2/request"
-BLUESCOPE= "search?q=bluescope&src=typd"
-SAP="search?q=%40sap&src=typd"
-ZUCKERBERG='search?q=Mark%20Zuckerberg&src=tyah'
-SAP_ANZ='search?q=sap%20anz&src=typd'
-MARCUS='marcustlim?lang=en'
-MICROSOFT='search?q=microsoft&src=typd'
-VILLAGE_ROADSHOW='search?q=village%20roadshow&src=typd'
+
 
 
 # Endpoint for API (fi required)
@@ -29,18 +24,24 @@ def analyse_text(text):
     make_request("")
 
 def sentiment_analysis(query):
-    tweets = scrape(query)
+    with open('searches.json') as f:
+        queries = json.load(f)
+
+    tweets = scrape(queries[query])
 
     # dictionary to store count of tweet sentiments
     sentiment_dict = defaultdict(int)
 
     for tweet in tweets:
-        sentiment = make_request(tweet)
+        sanitised_tweet = re.sub(r"http\S+", "", tweet)
+        sentiment = make_request(sanitised_tweet)
         sentiment_dict[sentiment] += 1
+        print(sanitised_tweet)
     print(sentiment_dict)
 
 
-def analyse_sentiment():
+def analyse_sentiment(sentiment):
+    total = sum(d.values())
     return
 
 
@@ -55,4 +56,4 @@ def make_request(text):
     sentiment = json.loads(response.raw)['results']['sentiment']
     return sentiment
 
-sentiment_analysis(VILLAGE_ROADSHOW)
+sentiment_analysis("marcus")
